4d4e59ad1d874086283ed7a2b785e01a6f984bfa
KalanaDemel2 <vmkdemel@gmail.com>
2018-09-16 10:18:51 +0530

    Handled promise

--- test/add.js
+++ test/add.js
@@ -9,9 +9,10 @@ const add = require('../lib/add');
 
 describe('Calculation Actions', (done) => {
 
-    it('Should add the inputs', () => {
+    it('Should add the inputs', async () => {
 
-        expect(add({'num1' : 1, 'num2' : 1})).to.deep.equal(2);
+        let res = await add({'num1' : 1, 'num2' : 1});
+        expect(res).to.equal(2);
 
     });
 
